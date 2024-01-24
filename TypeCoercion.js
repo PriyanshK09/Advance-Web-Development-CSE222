5742896fda24825e6867f00872c21ee581bec5e4
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-01-24 12:17:00 +0530

    TypeCoercion Updated

--- TypeCoercion.js
+++ TypeCoercion.js
@@ -3,5 +3,12 @@
 
 let a = 1 + "2";
 let b = 10 - "3";
+let c = true + 1;
+let d = 1 + 2 + "3";
+let e = "3" * 3;
+
 console.log(a); // 12
 console.log(b); // 7
+console.log(c); // 1
+console.log(d); // 33
+console.log(e); // 9
