88c70fe4dd37f8a1f101bda7a1acf4d1645440cd
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-01-24 12:02:55 +0530

    Code updated

--- Hoisting2.js
+++ Hoisting2.js
@@ -1,7 +1,19 @@
+// Hoisting with VAR
 console.log(x)
 var x = 10
 console.log(x)
 
+// Hoisting with LET
 console.log(y)
 let y = 10
 console.log(y)
+
+// Hoisting with FUNCTION
+console.log(z)
+function z() {
+    console.log('z')
+}
+console.log(z)
+
+// Hoisting Caveats
+// While the declarations are hoisted, the initializations are not.
--- TypeCoercion.js
+++ TypeCoercion.js
@@ -2,4 +2,6 @@
 // Coercion is the process of converting a value from one type to another (such as string to number, object to boolean, and so on).
 
 let a = 1 + "2";
+let b = 10 - "3";
 console.log(a); // 12
+console.log(b); // 7
