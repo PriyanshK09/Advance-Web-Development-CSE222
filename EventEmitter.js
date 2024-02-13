f5d46bdb643dc741e45ce113ed4c81bb99b06e22
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-02-13 09:41:52 +0530

    Add EventEmitter.js module for object communication in Node.js

---
+++ EventEmitter.js
@@ -0,0 +1,19 @@
+const EventEmitter = require('events');
+// Import the event module and create an instance of EventEmitter class
+
+const myEmitter = new EventEmitter();
+// Create an instance of the EventEmitter class. This instance will be used to emit and handle listeners.
+
+myEmitter.on('firstEvent', (arg1, arg2) => {    // Why arg1 and arg2 are important? : They are the parameters that are passed to the event handler.
+    // Event handler logic
+    console.log('First event has been emitted', arg1, arg2);
+});
+
+// The on method is used to register an event listener for a specific event. 
+// Multiple listeners can be attached to the same event.
+
+myEmitter.emit('firstEvent', 'Hello', 'HI!');
+// The emit method is used to emit an event.
+
+// Output
+// First event has been emitted arg1 arg2
