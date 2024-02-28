c504eeddf1c109ad4704d14b945954334be187b8
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-02-28 12:13:26 +0530

    Refactor code for improved performance and readability

---
+++ ExpressForm/expressForm.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+// Route
+app.get('/', (req, res) => {
+    fs.readFile('./ExpressForm/indexForm.html', (err, data) => {
+        if (err) {
+            res.status(500).send('Some error occurred');
+        }
+        res.setHeader('Content-Type', 'text/html');
+        res.send(data);
+    }
+    );
+});
+
+// Serving Route
+app.get('/submit', (req, res) => {
+    res.send('Form submitted successfully');
+    let data = req.query;
+    console.log('Name: ' + data.name);
+    console.log('Email: ' + data.email);
+    console.log('Rating: ' + data.rating);
+    console.log('Comment: ' + data.message);
+
+    // Writing to a file
+    fs.appendFile('./ExpressForm/formData.txt', `Name: ${data.name}, Email: ${data.email}, Rating: ${data.rating}, Comment: ${data.message}\n`, 'utf-8', (err) => {
+        if (err) {
+            console.log('Some error occurred');
+        }
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port https://localhost:${port}`);
+});
