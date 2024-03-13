6383502ce0a83b6719524fd99ce41230093fdaa9
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-03-13 12:39:54 +0530

    Update FormDataReciever.js to handle extended urlencoded data and serve index.html on root

--- Class-Express-Activity2/FormDataReciever.js
+++ Class-Express-Activity2/FormDataReciever.js
@@ -7,16 +7,19 @@
 // app.use(express.urlencoded({ extended: false }));
 
 const express = require('express');
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    const readStream = fs.createReadStream('public/index.html');
+    readStream.pipe(res);
+});
 
 app.post('/submit', (req, res) => {
     console.log(req.body);
-    res.send('Data received');
+    res.send('Data recieved');
 });
 
 app.listen(3000, () => {
