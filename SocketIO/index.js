b1a1ccc6cf8d56ad260b907e41faa655824d9fb2
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-03-19 10:39:59 +0530

    js updated with comments

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,16 +1,20 @@
+// Importing the required modules
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 const app = express();
+
+// Creating a server instance and attaching the express app to it
 const server = app.listen(3000, () => {
     console.log("Server Started");
 })
 const io = socketIO(server); // Attach Socket.IO to the server instance
 app.use(express.static(path.join(__dirname)));
 
+// Handling the connection event
 io.on('connection', (socket) => {
     console.log("A user has connected to server");
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // Emit 'chat message' instead of 'message'
+        io.emit('chat message', msg);
     })    
 })
